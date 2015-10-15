93b506444b4f50060a7a226335d925a651d5873b
Lucas <godkiller_nt@hammermaps.de>
2015-10-16 01:28:50 +0200

    Add VC2015 Projekt

---
+++ sohl_oppfor_sdk/Source Code/game_shared/vminmax.h
@@ -0,0 +1,19 @@
+//========= Copyright © 1996-2002, Valve LLC, All rights reserved. ============
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//=============================================================================
+
+#ifndef VMINMAX_H
+#define VMINMAX_H
+
+#if defined ( _MSC_VER )
+#pragma once
+#endif
+
+#define V_max(a, b)  (((a) > (b)) ? (a) : (b))
+#define V_min(a, b)  (((a) < (b)) ? (a) : (b))
+#define V_fabs(x)	 ((x) > 0 ? (x) : 0 - (x))
+
+#endif // VMINMAX_H
