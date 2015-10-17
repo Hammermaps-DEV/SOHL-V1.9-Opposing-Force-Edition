7c22b5a211f848da04b77152679315ff0d4f9892
Lucas <godkiller_nt@hammermaps.de>
2015-10-17 13:10:29 +0200

    Add Zombie Barney

---
+++ sohl_oppfor_sdk/Source Code/dlls/monster_zombie_barney.h
@@ -0,0 +1,47 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*
+*	This product contains software technology licensed from Id
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc.
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+
+#ifndef MONSTER_ZOMBIE_BARNEY_H
+#define MONSTER_ZOMBIE_BARNEY_H
+
+#include "monster_zombie.h"
+
+class CZombieBarney : public CZombie
+{
+public:
+	void Spawn(void);
+	void Precache(void);
+	void SetYawSpeed(void);
+	int  Classify(void);
+	void HandleAnimEvent(MonsterEvent_t *pEvent);
+	int IgnoreConditions(void);
+
+	void PainSound(void);
+	void AlertSound(void);
+	void IdleSound(void);
+	void AttackSound(void);
+
+	static const char *pAttackSounds[];
+	static const char *pIdleSounds[];
+	static const char *pAlertSounds[];
+	static const char *pPainSounds[];
+	static const char *pAttackHitSounds[];
+	static const char *pAttackMissSounds[];
+
+	// No range attacks
+	int TakeDamage(entvars_t *pevInflictor, entvars_t *pevAttacker, float flDamage, int bitsDamageType);
+};
+
+#endif // MONSTER_ZOMBIE_BARNEY_H
